722dbc0d703fe19433cdcd135501eaf0b9da5110
Your Name <you@example.com>
2025-07-01 13:00:48 +0200

    feat: add posts route and initial server setup with static files

---
+++ routes/posts.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const posts = require('../data/posts')
+
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3001
+const postsRouter = require('./routes/posts')
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+
+})
+
+app.get('/', (req, res) => {
+
+
+    res.send('WELCOME TO MY BLOG!')
+
+})
+
+app.use('/posts', postsRouter);
