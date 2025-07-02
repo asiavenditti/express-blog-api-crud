62251fd6611dd2469d06438ae523d0fcaed44ff9
Your Name <you@example.com>
2025-07-02 16:01:48 +0200

    feat: implement posts controller with CRUD operations and update routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,140 +2,26 @@ const express = require('express')
 const router = express.Router()
 const posts = require('../data/posts')
 const { error, log } = require('console')
+const postController = require('../controllers/postsController');
 
 
-/* index
 
-router.get('/', (req, res) => {
-
-
-    res.json(posts)
-    console.log(req);
-
-}) */
-
-
-// CRUD INDEX 
-
-router.get('/', (req, res) => {
-    let filteredPost = posts
-
-    if (req.query.tag) {
-        filteredPost = posts.filter((post) => {
-
-            return post.tags.includes(req.query.tag)
-        })
-    }
-
-    res.json(filteredPost)
-})
-
-
+// index
+router.get('/', postController.index);
 
 // show
+router.get('/:id', postController.show);
 
-router.get('/:id', (req, res) => {
-
-    const id = parseInt(req.params.id)
-
-    const post = posts.find((post) => {
-        return post.id === id
-    })
-
-
-    if (!post) {
-        res.status(404)
-        return res.json({
-            error: "Not Found",
-            message: "Post non trovato"
-        })
-    }
-    res.json(post)
-    console.log(post)
-})
 
 // delete
+router.delete('/:id', postController.destroy);
 
-router.delete('/:id', (req, res) => {
-
-    const id = parseInt(req.params.id)
-    const post = posts.find((post) => {
-        return post.id === id
-    })
-    console.log(post)
-    if (!post) {
-        return res.status(404).json({
-            message: `Post with ${id} not found`
-        })
-    }
-
-    posts.splice(posts.indexOf(post), 1)
-
-    console.log(posts);
-
-    res.sendStatus(204)
-
-
-
-})
-
-
-// store 
-
-
-router.post('/', (req, res) => {
-
-    console.log(req.body, 'Body')
-
-    const newId = posts[posts.length - 1].id + 1
-    console.log(newId);
-
-
-
-    const newPost = {
-        id: newId,
-        title: req.body.title,
-        content: req.body.content,
-        image: req.body.image,
-        tags: req.body.tags
-    };
-
-
-    posts.push(newPost)
-
-
-    console.log(posts)
-
-
-    res.status(201)
-    res.json(newPost)
-});
+// store
+router.post('/', postController.store);
 
 // update
+router.patch('/:id', postController.update);
 
-router.patch('/:id', (req, res) => {
-
-    const id = parseInt(req.params.id)
-
-    const post = posts.find(post => post.id === id)
-
-
-    if (!post) {
-        return res.status(404).json({
-            error: 'Not Found',
-            message: `Post non trovato`
-        })
-    }
-
-    post.title = req.body.title
-    post.image = req.body.image
-    post.content = req.body.content
-    post.tags = req.body.tags
-
-
-    console.log(posts)
-    res.json(post)
-})
 
 
 
